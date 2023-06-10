0c3c04a0ee0b4efe8ca6e55e2c7356d6b56bf6fb
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-06-11 00:30:51 +0430

    Add style to the Home page!

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,19 @@
 import { Link } from "react-router-dom";
-import styles from "./home.module.css"
+import styles from "./home.module.css";
 
 const HomePage = () => {
   return (
-    <div className = {styles.homeMianDiv}>
-      <h1>Welcome to My Podcast and Book App!</h1>
-      <h2>Featured Books</h2>
+    <div className={styles.homeMianDiv}>
       <div className={styles.rootDivOfPodBook}>
-        <Link to="/all-books">
-          <div className={styles.PodBookInHomePage}>
-            List All Books
-          </div>
-        </Link>
         <div className={styles.PodBookInHomePage}>
-          List All Podcasts
+          <div alt="books" className={styles.img1} />
+          <Link to="/all-books">
+            <button>View all books</button>
+          </Link>
+        </div>
+        <div className={styles.PodBookInHomePage}>
+          <div alt="podcasts" className={styles.img2} />
+          <button>View all books</button>
         </div>
       </div>
     </div>
