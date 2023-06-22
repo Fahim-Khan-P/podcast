366bb84f2ce5e41e3500cc9c528980d1fa2b658b
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-06-22 14:37:05 +0430

    Add styles!

--- src/components/PodcastsCategory.jsx
+++ src/components/PodcastsCategory.jsx
@@ -1,18 +1,25 @@
 import styles from './Category.module.css'
+import sound from '../assets/audio.mp3'
+
 
 function PodcastCategory() {
+  const handleMouseOver = () => {
+    const audio = new Audio(sound);
+    audio.play();
+  };
+
   return (
     <div className={styles.mainDiv}>
-      <div className={styles.card}>
-        <div className={styles.img}></div>
+      <div className={styles.card} onMouseOver={handleMouseOver}>
+        <div className={styles.podcastImg}></div>
         <h6>Photography</h6>
       </div>
-      <div className={styles.card}>
-        <div className={styles.img}></div>
+      <div className={styles.card} onMouseOver={handleMouseOver}>
+        <div className={styles.podcastImg}></div>
         <h6>Childreens</h6>
       </div>
-      <div className={styles.card}>
-        <div className={styles.img}></div>
+      <div className={styles.card} onMouseOver={handleMouseOver}>
+        <div className={styles.podcastImg}></div>
         <h6>Hobbies</h6>
       </div>
     </div>
